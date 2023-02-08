de55834d2240a1bc3a91fa1250f72d60bef094ab
jnovais1 <109621297+jnovais1@users.noreply.github.com>
2023-02-07 21:22:31 -0300

    Learning getters and setters

---
+++ funcionario.php
@@ -0,0 +1,56 @@
+<?php
+
+class Funcionario {
+    public $nome;
+    public $telefone;
+    public $numFilhos;
+
+function getNumFilhos(){
+    echo $this->numFilhos;
+}
+function setNumFilhos($numFilhos) {
+    $this->numFilhos = $numFilhos;
+}
+function getNome(){
+    echo $this->nome;
+}
+function setNome($nomeFunc) {
+    $this->nome = $nomeFunc;
+}
+function getTelefone(){
+    echo $this->telefone;
+}
+function setTelefone($telefone) {
+    $this->telefone = $telefone;
+}
+function resumo(){
+    echo "$this->nome possui $this->numFilhos filhos.<br>
+    telefone: $this->telefone";
+}
+function __get($variavel){
+    echo $this->$variavel;
+}
+function __set($variavel, $valor){
+    $this->$variavel = $valor;
+}
+}
+$joao = new Funcionario();
+
+$p = new Funcionario();
+$p->setNome("Joshua");
+$p->setNumFilhos(3);
+$p->setTelefone("3333-3333");
+echo $p->getNome();
+echo "<br>";
+echo $p->getNumFilhos();
+echo "<br>";
+echo $p->getTelefone();
+echo "<br>";
+echo $p->resumo();
+echo "<br>";
+
+echo $p->__get('nome');
+echo "<br>";
+$p->__set('telefone', '5');
+echo $p->__get('telefone')
+?>
